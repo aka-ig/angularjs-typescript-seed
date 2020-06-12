a07cdc3f7531c14f1425974ed3c3c6a2a69458ec
luodong89 <luodong89@luodong89s-MacBook-Pro.local>
2020-06-12 00:04:03 -0400

    Add font-awesome

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as angular from 'angular';
 import { ModuleKarso } from './karso/karso.module';
+import './style.scss';
 
 angular.bootstrap(document, [ModuleKarso]);
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,19 @@ module.exports = {
             // Translates CSS into CommonJS
             // Compiles Sass to CSS
             { test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] },
+            { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
         ]
     },
     devServer: {
